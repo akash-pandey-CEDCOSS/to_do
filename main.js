92135bcd81de2b6f5a3e7c306a513fb1f54453ea
akash1297 <akash.zerox505@gmail.com>
2020-12-08 11:52:59 +0530

    add changes

--- main.js
+++ main.js
@@ -1,21 +1,39 @@
 $(document).ready(function () {
     $("#submit_").click(function () {
-
-
-        var task_ = $("#input_todo").val();
-        $("ul").append("<li><input type='checkbox'><input type='text'>" + task_ + "<button type='button' class='edit'>Edit</button><button type='button' class='delete'>Delete</button></li>");
-
+        // Adding task 
+        var task_ = $(".input_todo").val();
+        $("#ul1").append(`<li><input type='checkbox' class="checkd"><input type='text' class='val_'><span class='task_done'>${task_}</span><button type='button' class='edit'>Edit</button><button type='button' class='delete'>Delete</button><button type='button' class='save'>Save</button></li>`);
+        // delete Function
         $(".delete").click(function () {
+            $(this).parent("li").toggleClass("stroked").fadeOut("slow");
+        });
+        // Checked Unchecked
+        $(".checkd").click(function () {
+            if ($(this).prop("checked") == true) {
+                var a = $(this).parent("li");
+                $("#ul2").append(a);
+            }
+        })
+        $(".checkd").click(function () {
+            if ($(this).prop("checked") == false) {
+                var b = $(this).parent("li");
+                $("#ul1").append(b);
+            }
+        })
+        // edit function
+        var toEdit = "";
+        $(".edit").click(function () {
+            var c = $(this).siblings(".task_done").html();
+            
+            $(this).siblings(".val_").val(c);
 
-            $(this).parent("li").remove();
 
         });
-        $(".edit").click(function(){
-
+        // save a edit
+        $(".save").click(function () {
+            var save_ = $(this).siblings(".val_").val().trim();
+            
+            $(this).siblings(".task_done").html(save_);
         })
-
-
     });
-
-
 });
