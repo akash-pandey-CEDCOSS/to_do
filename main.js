d0e859ea5a84b9860c56c218c6000821d2f85d9e
akash1297 <akash.zerox505@gmail.com>
2020-12-08 13:50:27 +0530

    add changes

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ $(document).ready(function () {
         // Adding task 
         var task_ = $(".input_todo").val();
         if(task_!=""){
-        $("#ul1").append(`<li><input type='checkbox' class="checkd"> <span class='task_done'>${task_}</span> <input type='text' class='val_'> <button type='button' class='edit'>Edit</button><button type='button' class='delete'>Delete</button><button type='button' class='save'>Save</button></li>`);
+        $("#ul1").append(`<li> <span class='task_done'>${task_}</span> <input type='text' class='val_'> <button type='button' class='edit'>Edit</button><button type='button' class='delete'>Delete</button><button type='button' class='save'>Save</button> <input type='checkbox' class="checkd"></li>`);
         $(".input_todo").val("");
         // delete Function
+        $(".val_").hide();
+        $(".save").hide();
         $(".delete").click(function () {
             $(this).parent("li").toggleClass("stroked").fadeOut("slow");
         });
@@ -14,6 +16,9 @@ $(document).ready(function () {
             if ($(this).prop("checked") == true) {
                 var a = $(this).parent("li");
                 $("#ul2").append(a);
+                $(".val_").hide();
+                $(".edit").hide();
+                $(".save").hide();
             }
         })
         $(".checkd").click(function () {
@@ -25,6 +30,8 @@ $(document).ready(function () {
         // edit function
         var toEdit = "";
         $(".edit").click(function () {
+            $(".val_").show();
+            $(".save").show();
             var c = $(this).siblings(".task_done").html();
             
             $(this).siblings(".val_").val(c);
@@ -36,6 +43,7 @@ $(document).ready(function () {
             var save_ = $(this).siblings(".val_").val().trim();
             
             $(this).siblings(".task_done").html(save_);
+            $(".val_").hide();
         });
     }
 
